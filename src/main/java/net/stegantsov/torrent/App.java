07f27ac87391b623cc457e6f3062937c556d2ef4
stegan <andrey@stegantsov.net>
2013-10-20 00:30:55 +0400

    mvn empty project

---
+++ src/main/java/net/stegantsov/torrent/App.java
@@ -0,0 +1,13 @@
+package net.stegantsov.torrent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
